d8ccc54a1fa5e2d71c083973b3cda99bd762c9f7
ndrcstl <148858656+ndrcstl@users.noreply.github.com>
2024-07-11 18:25:41 +0800

    Update Mod3.sol

--- Mod3.sol
+++ Mod3.sol
@@ -1,64 +1,25 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-contract ERC20Mod3Proj {
-    string public name;
-    string public symbol;
-    uint8 public decimals;
-    uint256 public totalSupply;
-    mapping(address => uint256) public balanceOf;
-    address public owner;
+import "@openzeppelin/contracts@4.9.0/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts@4.9.0/token/ERC20/extensions/ERC20Burnable.sol";
+import "@openzeppelin/contracts@4.9.0/access/Ownable.sol";
 
-    event Transfer(address indexed from, address indexed to, uint256 value);
-    event Mint(address indexed to, uint256 value);
-
-    constructor() {
-        name = "EbokCoin";
-        symbol = "EBC";
-        decimals = 18;
-        totalSupply = 1000 * 10** uint256(decimals);
-        balanceOf[msg.sender] = totalSupply;
-        owner = msg.sender;
-
-        address[] memory addresses = new address[](3);
-        addresses[0] = 0xAb8483F64d9C6d1EcF9b849Ae677dD3315835cb2;
-        addresses[1] = 0xCA35b7d915458EF540aDe6068dFe2F44E8fa733c;
-        addresses[2] = 0x4B20993Bc481177ec7E8f571ceCaE8A9e22C02db;
-        uint256 tokensPerAddress = 1000 * 10**uint256(decimals);
-
-        for (uint256 i = 0; i < addresses.length; i++) {
-            balanceOf[addresses[i]] += tokensPerAddress;
-            totalSupply += tokensPerAddress;
-            emit Transfer(address(0), addresses[i], tokensPerAddress);
-            emit Mint(addresses[i], tokensPerAddress);
-        }
+contract myToken is ERC20, ERC20Burnable, Ownable {
+    constructor(string memory name, string memory symbol, uint256 initialSupply) ERC20(name, symbol) {
+        _mint(msg.sender, initialSupply);
     }
 
-    modifier onlyOwner() {
-        require(msg.sender == owner, "Only the contract owner can call this function");
-        _;
+    function mint(address account, uint256 amount) public onlyOwner {
+        _mint(account, amount);
     }
 
-    function mint(address _to, uint256 _amount) public onlyOwner {
-        require(_to != address(0), "Invalid address");
-        totalSupply += _amount;
-        balanceOf[_to] += _amount;
-        emit Mint(_to, _amount);
-        emit Transfer(address(0), _to, _amount);
+    function burnFrom(address account, uint256 amount) public override  {
+        _burn(account, amount); // Using _burn directly
     }
 
-    function burn(uint256 _amount) public {
-        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
-        balanceOf[msg.sender] -= _amount;
-        totalSupply -= _amount;
-        emit Transfer(msg.sender, address(0), _amount);
-    }
-
-    function transfer(address _to, uint256 _amount) public {
-        require(_to != address(0), "Invalid address");
-        require(balanceOf[msg.sender] >= _amount, "Insufficient balance");
-        balanceOf[msg.sender] -= _amount;
-        balanceOf[_to] += _amount;
-        emit Transfer(msg.sender, _to, _amount);
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
     }
 }
